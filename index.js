8c9dc1e68be5cbe21a163a91272577a818a70be2
arimetson <128273620+arimetson@users.noreply.github.com>
2024-01-25 17:40:55 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
-
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
-});
-
 bot.status({
   text: "Aina -  とてもかわいい !",
   type: "PLAYING",
   url: "https://www.youtube.com/c/BeingYT",
 });
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+
 
 bot.connect(); // Get the bot to connect to Discord
